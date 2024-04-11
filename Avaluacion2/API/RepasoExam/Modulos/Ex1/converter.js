d007576594fd0ee854688bb1cfaa22f62ff26b23
Sam <samuel.fuentes.baio@gmail.com>
2024-04-11 14:08:25 +0200

    saassasaasassaassaasdx

--- Avaluacion2/API/RepasoExam/Modulos/Ex1/converter.js
+++
@@ -1,7 +0,0 @@
-// Definir la función converterAMaiusculas
-function converterAMaiusculas(cadena) {
-    return cadena.toUpperCase();
-}
-
-// Exportar la función para que esté disponible fuera del módulo
-module.exports = converterAMaiusculas;
