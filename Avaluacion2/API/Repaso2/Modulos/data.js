d5c648f937981172073d2f0b2db082f17b86c76d
Sam <samuel.fuentes.baio@gmail.com>
2024-04-16 13:50:27 +0200

    ejercicios

--- Avaluacion2/API/Repaso2/Modulos/data.js
+++
@@ -1,9 +0,0 @@
-export function obterData() {
-    let data = new Date();
-    let dia = data.getDate();
-    let mes = data.getMonth() + 1; // Lembrar que os meses comezan en 0
-    let ano = data.getFullYear();
-  
-    return `${dia}/${mes < 10 ? '0' + mes : mes}/${ano}`;
-  }
-  
--- Avaluacion2/API/Repaso2/Modulos/hora.js
+++
@@ -1,8 +0,0 @@
-export function obterHora() {
-    let data = new Date();
-    let hora = data.getHours();
-    let minutos = data.getMinutes();
-  
-    return `${hora}:${minutos < 10 ? '0' + minutos : minutos}`;
-  }
-  
